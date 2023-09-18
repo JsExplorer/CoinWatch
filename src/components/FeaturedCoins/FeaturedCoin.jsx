4f0daf4f0b870215a5a976292fa6603a0cdf5db5
rsaf <rsaf@rsafs-MacBook-Pro.local>
2023-09-18 15:12:19 +0800

    first commit

--- src/components/FeaturedCoins/FeaturedCoin.jsx
+++ src/components/FeaturedCoins/FeaturedCoin.jsx
@@ -2,7 +2,8 @@ import { FiArrowUp, FiArrowDown } from 'react-icons/fi'
 
 const FeaturedCoin = ( {coin} ) => {
     return (
-    <div className='card'>
+    <div className='grid grid-cols-3 sm:grid-cols-4 font-semibold p-2 rounded border-gray-200 border-5 hover:bg-green-200'>
+        <div className='flex items-center gap-2 w-full'></div>
         <img src={coin.image} alt=''/>
         <h3>{coin.name}</h3>
         <p>${coin.current_price.toLocaleString()}</p>
--- src/components/TrendingCoins/TrendingCoin.jsx
+++ src/components/TrendingCoins/TrendingCoin.jsx
@@ -1,7 +1,7 @@
 
 const TrendingCoin = ({ coin }) => {
   return (
-    <div className='font-light border-gray-150 border-2 rounded hover:bg-gray-150'>
+    <div className='font-light border-gray-200 border-2 rounded hover:bg-gray-200'>
         <div className='flex items-center gap-2'>
             <span className='font-semibold'>{coin.item.score+1}.</span>
             <img className='w-8' src={coin.item.small} alt=''></img>
